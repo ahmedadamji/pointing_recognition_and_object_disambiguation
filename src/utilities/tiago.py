6d11e9be96630627fabd12365a1a8e1e05ffacd0
therobomaker <el17ana@leeds.ac.uk>
2021-01-18 21:51:53 +0000

    Added code so that now

--- src/utilities/tiago.py
+++ src/utilities/tiago.py
@@ -1,85 +1,45 @@
 #!/usr/bin/python
 import rospy
 import actionlib
-from rospy import ROSException, ROSInterruptException
-import rosnode
-
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from play_motion_msgs.msg import PlayMotionAction, PlayMotionGoal
-from control_msgs.msg import PointHeadAction, PointHeadGoal
-from geometry_msgs.msg import Point, Pose, Quaternion, Twist, PoseWithCovarianceStamped, Vector3
-from nav_msgs.msg import OccupancyGrid
-
-from pal_interaction_msgs.msg import TtsAction, TtsGoal
+from trajectory_msgs.msg import JointTrajectory
+from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
 
 
 class Tiago:
     def __init__(self):
 
-        self.move_base_goal_sent = False	# if goal is sent, we need to cancel before shutting down
+        # if goal is sent, we need to cancel before shutting down
         self.play_motion_goal_sent = False
 
         rospy.on_shutdown(self.shutdown)
-
-        self.rate = rospy.Rate(10)
-
-        self.robot_pose = None
-        self.snf_requests = []
-
-		# set linear and angular velocities
-        self.velocity = Twist()
-		# publish and subcribe to relevant topics
-        self.velocity_publisher = rospy.Publisher('/mobile_base_controller/cmd_vel', Twist, queue_size=10)
-
-        self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-        self.move_base.wait_for_server(rospy.Duration(5))
-        rospy.loginfo("The move_base action server is up")
-
-        self.play_motion = actionlib.SimpleActionClient("play_motion", PlayMotionAction)
+        self.enable_torso = actionlib.SimpleActionClient("/torso_controller/follow_joint_trajectory", FollowJointTrajectoryAction)
+        self.play_motion = actionlib.SimpleActionClient("/play_motion", PlayMotionAction)
         self.play_motion.wait_for_server(rospy.Duration(5))
         rospy.loginfo("The play_motion action server is up")
 
-        self.tts_client = actionlib.SimpleActionClient('/tts', TtsAction)
-        self.tts_client.wait_for_server(rospy.Duration(5))
-        rospy.loginfo("The tts action server is up")
-
-        self.point_head = actionlib.SimpleActionClient('/head_controller/point_head_action', PointHeadAction)
-        self.point_head.wait_for_server(rospy.Duration(5))
-        rospy.loginfo("The point_head action server is up")
+        # self.play_motion_client = actionlib.SimpleActionClient("/play_motion", PlayMotionAction)
+        # self.play_motion_client.wait_for_server(rospy.Duration(5))
+        # rospy.loginfo("The /play_motion action server is up")
 
 
-    def move(self, linear=(0,0,0), angular=(0,0,0)):
-        self.velocity.linear.x = linear[0] 	# Forward or Backward with in m/sec.
-        self.velocity.linear.y = linear[1]
-        self.velocity.linear.z = linear[2]
-
-        self.velocity.angular.x = angular[0]
-        self.velocity.angular.y = angular[1]
-        self.velocity.angular.z = angular[2] 	# Anti-clockwise/clockwise in radians per sec
-
-        self.velocity_publisher.publish(self.velocity)
-
-
-    # True passed so that we wait for the result before any other code is performed
-    def lift_torso_head_default(wait=True):
+    def lift_torso_head_default(self, wait=False):
         # lift torso high and head to default
-        play_motion_client = actionlib.SimpleActionClient('/play_motion', PlayMotionAction)
-        play_motion_client.wait_for_server()
+        self.play_motion_goal_sent = True
+        torso_goal = FollowJointTrajectoryGoal()
         pm_goal = PlayMotionGoal('back_to_default', True, 0)
         test_goal = PlayMotionGoal()
         print test_goal.priority
-        play_motion_client.send_goal(pm_goal)
-        rospy.loginfo('play motion: back to default')
+        #self.play_motion.send_goal(torso_goal)
+        self.play_motion.send_goal(pm_goal)
 
         if wait:
-            play_motion_client.wait_for_result()
+            self.play_motion.wait_for_result()
+
+        rospy.loginfo("play motion: back_to_default")
 
 
     def shutdown(self):
-        if self.move_base_goal_sent:
-            self.move_base.cancel_goal()
-            rospy.loginfo("Stop Robot")
-            rospy.sleep(1)
 
         if self.play_motion_goal_sent:
             self.play_motion.cancel_goal()
